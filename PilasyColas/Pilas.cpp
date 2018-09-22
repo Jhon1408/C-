04aee1f03ca5810b3d1704493dd2e612bfc35e26
Jhonatan Ospina <wother1409@gmail.com>
2018-09-22 11:03:44 -0500

    Agregando carpeta Pilas y colas, con el archivo sin testear de Pilas.cpp

---
+++ PilasyColas/Pilas.cpp
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define infNegative -2147483647;
+
+struct node {
+  int key;
+  struct node *next;
+}
+
+struct node *Push(struct node *top, int element) {
+  struct node *newNode;
+  newNode = (struct node *) malloc(sizeof(struct node));
+  newNode->key = element;
+  newNode->next = top;
+  top = newNode;
+  return top;
+}
+
+int Pop(struct node **top) {
+  struct node *q;
+  int element = infNegative;
+  if(*top == NULL)
+    printf("Nope, vacio karnal.\n");
+  else {
+    q = *top;
+    element = q->key;
+    *top = (*top)->next;
+    free(q);
+  }
+  return element;
+}
+
+int main() {
+  struct node *top;
+  int operacion, element;
+  top = NULL;
+  while (scanf("%d", &operacion) != EOF) {
+    if (operacion == 1) {
+      scanf("%d", &element);
+      top = Push(top,element);
+    } else {
+      if (operacion == 2) {
+        element = Pop(&top);
+        if(element != infNegative) {
+          printf("%d\n", element);
+        }
+      } else {
+        printf("Bad operation, use:\n1: Push\n2: Pop\n");
+      }
+    }
+  }
+  return 0;
+}
