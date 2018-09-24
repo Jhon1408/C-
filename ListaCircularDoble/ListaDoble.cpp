884d356ac98177e40448397c1108069dcbc37566
Jhonatan Ospina <wother1409@gmail.com>
2018-09-24 10:56:06 -0500

    Lista doble Josephus Lottery (Sin terminar).

--- ListaCircularDoble/ListaDoble.cpp
+++ ListaCircularDoble/ListaDoble.cpp
@@ -80,7 +80,82 @@ void PrintFromLeftToRight(struct node *tail) {
   }
 }
 
+//---------------------------------------------------------------------------------------------------------
+int ganador(struct node *tail, int k) {
+   struct node *q;
+   int index, result;
+   while(tail != tail -> next) {
+      for(index = 1; index < k; index++)
+         tail = tail -> next;
+      q = tail -> next;
+      tail -> next = q -> next;
+      free(q);
+   }
+   result = tail -> key;
+   free(tail);
+   return result;
+}
+
+struct node *CreateCircularListDoublyLinked(int n) {
+  struct node *tail;
+  tail = NULL;
+  for (int i = 2; i <= n; i++) {
+    tail = InsertInCircularBoundlyLinkedList(tail, i);
+  }
+  return tail;
+}
+
+int solver(int n) {
+  struct node *tail;
+  int m, winner;
+  for (int i = 1; ; i++) {
+    tail = CreateCircularListDoublyLinked(n);
+    winner = ganador(tail, i);
+    if (winner == 2) {
+      m = i;
+      break;
+    }
+  }
+  return m;
+}
+
+int JosephusLottery(struct node *tail, int k) {
+  struct node *q;
+  int index, clock = 0, result;
+  tail = tail->next;
+  while (tail != tail->next) {
+    if (clock % 2 == 0) {
+      for (index = 1; index < k; index++) {
+        tail = tail->next;
+      }
+      q = tail;
+      tail = tail->next;
+      q->prev->next = q->next;
+      q->next->prev = q->prev;
+      free(q);
+      clock++;
+    } else {
+      for (index = 1; index < k; index++) {
+        tail = tail->prev;
+      }
+      q = tail;
+      tail = tail->prev;
+      q->prev->next = q->next;
+      q->next->prev = q->prev;
+      free(q);
+      clock++;
+    }
+  }
+  result = tail->key;
+  free(tail);
+  return result;
+}
+
 int main() {
-  
+  int n, k;
+  while (scanf("%d %d", &n, &k) && ((n != 0) && (k != 0))) {
+    m = solver(n, k);
+    printf("%d\n", m);
+  }
   return 0;
 }
