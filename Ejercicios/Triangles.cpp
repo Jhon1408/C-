2b64df8da1c5c96b16265d4e0e78e4dd5f11bfd1
Jhonatan Ospina <wother1409@gmail.com>
2018-10-01 09:55:16 -0500

    comit

--- Ejercicios/Triangles.cpp
+++ Ejercicios/Triangles.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-using namespace std;
 
 int main() {
   unsigned long long n, x, g;
@@ -10,9 +8,9 @@ int main() {
     x = ((sqrt(1+(8*(double)n)))-1)/2;
     g = (x*(x+1))/2;
     if (g == n) {
-      cout << "YES" << '\n';
+      printf("YES\n");
     } else {
-      cout << "NO" << '\n';
+      printf("NO\n");
     }
   }
   return 0;
